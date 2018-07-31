83d8c5e5e47290f381bbb808d090248b2f1d72ad
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-07-31 18:37:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,9 +8,11 @@ var chocolateBars = [
   function addElementToBeginningOfArray(arr, arg){
     //add arg to beginning of arr
     //should not alter the original arr
+    return arr.unshift(arg);
+    
   }
   
   function destructivelyAddElementToBeginningOfArray(arr, arg){
     //add arg to beginning of arr
-    //should alter the original array
+    //should alter the original arr
   }
