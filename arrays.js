e4e9b41a9e9e6fb456269691866558269115df0b
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-07-31 18:40:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,8 @@ var chocolateBars = [
   'skittles'
   ];
   
+  //Both functions take two parameters, an array and an element to add to the beginning of the array, and both functions should add the element to the beginning of the array and then return the whole array.
+  
   function addElementToBeginningOfArray(arr, arg){
     //add arg to beginning of arr
     //should not alter the original arr
