72ad83efb1b5d5f64fb537fbb0139c9f9d2d5c36
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-07-31 18:42:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,14 +8,12 @@ var chocolateBars = [
   //Both functions take two parameters, an array and an element to add to the beginning of the array, and both functions should add the element to the beginning of the array and then return the whole array.
   
   function addElementToBeginningOfArray(arr, arg){
-    //add arg to beginning of arr
     //should not alter the original arr
-    arr.unshift(arg);
-    return arr;
+    var newArr = arr.unshift(arg);
+    return newArr;
   }
   
   function destructivelyAddElementToBeginningOfArray(arr, arg){
-    //add arg to beginning of arr
     //should alter the original arr
     arr = arr.unshift(arg);
     return arr;
