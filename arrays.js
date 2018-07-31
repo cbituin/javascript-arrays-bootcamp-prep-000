70ba3cf4e29c80cad6f2bf79a850b3bdbfbc08e9
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-07-31 18:34:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,10 +5,10 @@ var chocolateBars = [
   'skittles'
   ];
   
-  function addElementToBeginningOfArray(){
+  function addElementToBeginningOfArray(arr, arg){
     
   }
   
-  function destructivelyAddElementToBeginningOfArray(){
+  function destructivelyAddElementToBeginningOfArray(arr, arg){
     
   }
